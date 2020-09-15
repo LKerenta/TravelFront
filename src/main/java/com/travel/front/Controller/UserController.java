20c5549be1ae5158a24c02ccb1ca3358759cd928
wxj <825754896@qq.com>
2020-09-15 16:53:49 +0800

    e

--- src/main/java/com/travel/front/Controller/UserController.java
+++ src/main/java/com/travel/front/Controller/UserController.java
@@ -1,6 +1,16 @@
 package com.travel.front.Controller;
 
 
-//For Tourist
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
 public class UserController {
+
+
 }

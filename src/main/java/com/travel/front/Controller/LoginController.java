2f9f0226e35244f254dcf2a695147187f81a9074
Kerenta <y1215573351@gmail.com>
2020-09-15 09:33:48 +0800

    Login参数传递

--- src/main/java/com/travel/front/Controller/LoginController.java
+++ src/main/java/com/travel/front/Controller/LoginController.java
@@ -1,9 +1,13 @@
 package com.travel.front.Controller;
 
 
+import com.travel.front.Entity.Franchise;
+import com.travel.front.Entity.Manager;
+import com.travel.front.Entity.User;
 import com.travel.front.Service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,11 +26,29 @@ public class LoginController {
         return "login";
     }
     @PostMapping("/index")
-    public String userLoginPressed(UserType userType){
+    public String userLoginPressed(UserType userType, Model model){
 
         Integer i = loginService.login(userType);
-        if(i == 1)
+
+
+        if(i == 0)
+        {
+            Manager manager = loginService.RM(userType);
+            model.addAttribute("Info",manager);
+            return "index";
+        }
+        else if(i == 1)
+        {
+            Franchise franchise = loginService.RF(userType);
+            model.addAttribute("Info",franchise);
+            return "index_F";
+        }
+        else if(i == 2)
+        {
+            User user = loginService.RT(userType);
+            model.addAttribute("Info",user);
             return "index_T";
+        }
         else return "login";
     }
 }
--- src/main/java/com/travel/front/Mapper/LoginMapper.java
+++ src/main/java/com/travel/front/Mapper/LoginMapper.java
@@ -1,5 +1,8 @@
 package com.travel.front.Mapper;
 
+import com.travel.front.Entity.Franchise;
+import com.travel.front.Entity.Manager;
+import com.travel.front.Entity.User;
 import com.travel.front.Entity.UserType;
 import org.apache.ibatis.annotations.*;
 
@@ -7,4 +10,13 @@ import org.apache.ibatis.annotations.*;
 public interface LoginMapper {
     @Select("SELECT * from usertype WHERE ID=#{ID} and Password=#{Password}")
     UserType login(UserType userType);
+
+    @Select("SELECT * FROM user WHERE UserID=#{ID} AND Password=#{Password}")
+    User RT(UserType userType);
+
+    @Select("SELECT * FROM franchise WHERE FranID=#{ID} AND Password=#{Password}")
+    Franchise RF(UserType userType);
+
+    @Select("SELECT * FROM manager WHERE MaID=#{ID} AND Password=#{Password}")
+    Manager RM(UserType userType);
 }

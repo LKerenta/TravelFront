d57942681af1a7b7d158defb545a7cf23ecc2408
Kerenta <y1215573351@gmail.com>
2020-09-18 08:55:27 +0800

    Functions
    Register, List orders and items by franchise

--- src/main/java/com/travel/front/Service/GoodsService.java
+++ src/main/java/com/travel/front/Service/GoodsService.java
@@ -1,4 +1,19 @@
 package com.travel.front.Service;
 
+import com.travel.front.Entity.Goods;
+import com.travel.front.Entity.ScenicSpot;
+
+import java.sql.Date;
+import java.util.List;
+
 public interface GoodsService {
+    List<Goods> getAllGoods();
+    List<Goods> getGoodsBySSID(ScenicSpot scenicSpot);
+    List<Goods> getGoodsByDate(Date date);
+    List<Goods> getGoodsByName(String name);
+    List<Goods> getGoodsByMeals(int Meals);
+    List<Goods> getGoodsByFranID(int ID);
+    Integer CreateGoods(Goods goods);
+    Integer updateGoods(Goods goods);
+    Integer deleteGoods(Integer ID);
 }

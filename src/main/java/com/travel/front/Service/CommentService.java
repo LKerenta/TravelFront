aaf82b5cbf94582e6f578ec8d6c277da0217f8b1
Kerenta <y1215573351@gmail.com>
2020-09-18 10:48:52 +0800

    int to integer

--- src/main/java/com/travel/front/Service/CommentService.java
+++ src/main/java/com/travel/front/Service/CommentService.java
@@ -1,4 +1,15 @@
 package com.travel.front.Service;
 
+import com.travel.front.Entity.Comment;
+
+import java.util.List;
+
 public interface CommentService {
+    List<Comment> getAllComments();
+    List<Comment> getCommentByUser(Integer ID);
+    List<Comment> getCommentByItem(Integer ID);
+    List<Comment> getCommentByKeyword(String search);
+    Integer writeComment(Comment comment);
+    Integer updateComment(Comment comment);
+    Integer deleteComment(Comment comment);
 }
